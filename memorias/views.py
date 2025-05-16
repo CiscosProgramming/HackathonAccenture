ea495452d61b73d0b9bb38b4f4eb2fbc7b2663c7
Alexandre Venâncio <alexandredsvenancio@gmail.com>
2025-05-16 14:48:24 +0100

    first commit

--- memorias/views.py
+++ memorias/views.py
@@ -1,6 +1,12 @@
+from audioop import reverse
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import SignUpForm, LoginForm
+from django.contrib import messages
+
+from .models import *
 
 
 def chatBot_view(request):
@@ -14,14 +20,32 @@ def chatBot_view(request):
 
 def signup_view(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/chatBot')  # Redireciona para a página principal após registo
-    else:
-        form = SignUpForm()
-    return render(request, 'signUp.html', {'form': form})
+        nome = request.POST.get('nome')
+        phone_number = request.POST.get('phoneNumber')
+        idade = request.POST.get('idade')
+        pin = request.POST.get('pin')
+
+        if CustomUser.objects.filter(phone_number=phone_number).exists():
+            messages.error(request, 'Número de telefone já cadastrado.')
+            return render(request, 'signup.html')
+
+        try:
+            idade_int = int(idade)
+        except (ValueError, TypeError):
+            messages.error(request, 'Idade inválida.')
+            return render(request, 'signup.html')
+
+        user = CustomUser.objects.create_user(
+            phone_number=phone_number,
+            pin=pin,
+            name=nome,
+            age=idade_int
+        )
+
+        login(request, user)
+        return redirect(reverse('memorias:chatBot'))
+
+    return render(request, 'signup.html')
 
 def login_view(request):
     if request.method == 'POST':
